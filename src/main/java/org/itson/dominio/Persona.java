ec648ac9204147a3cdf83b5b2ce0413e119448ce
MarioZDR <mario.diaz233259@potros.itson.edu.mx>
2023-03-28 22:01:16 -0700

    Mapeo de la entidad Persona

--- src/main/java/org/itson/dominio/Persona.java
+++ src/main/java/org/itson/dominio/Persona.java
@@ -5,19 +5,67 @@
 package org.itson.dominio;
 
 import java.io.Serializable;
+import java.util.Calendar;
 import javax.persistence.*;
 
 /**
  *
- * @author mario
+ * @author 233133_233259
  */
 @Entity
+@Table(name = "personas")
 public class Persona implements Serializable {
 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "discapacitado", nullable = false)
+    private boolean discapacitado;
+    
+    @Column(name = "fecha_nacimiento", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar fecha_nacimiento;
+    
+    @Column(name = "telefono", nullable = false, length = 20)
+    private String telefono;
+    
+    @Column(name = "rfc", nullable = false, length = 15)
+    private String rfc;
+    
+    @Column(name = "nombres", nullable = false, length = 40)
+    private String nombres;
+    
+    @Column(name = "apellido_paterno", nullable = false, length = 30)
+    private String apellido_paterno;
+    
+    @Column(name = "apellido_materno", nullable = false, length = 30)
+    private String apellido_materno;
+
+    public Persona() {
+    }
+
+    public Persona(boolean discapacitado, Calendar fecha_nacimiento, String telefono, String rfc, String nombres, String apellido_paterno, String apellido_materno) {
+        this.discapacitado = discapacitado;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.telefono = telefono;
+        this.rfc = rfc;
+        this.nombres = nombres;
+        this.apellido_paterno = apellido_paterno;
+        this.apellido_materno = apellido_materno;
+    }
+
+    public Persona(Long id, boolean discapacitado, Calendar fecha_nacimiento, String telefono, String rfc, String nombres, String apellido_paterno, String apellido_materno) {
+        this.id = id;
+        this.discapacitado = discapacitado;
+        this.fecha_nacimiento = fecha_nacimiento;
+        this.telefono = telefono;
+        this.rfc = rfc;
+        this.nombres = nombres;
+        this.apellido_paterno = apellido_paterno;
+        this.apellido_materno = apellido_materno;
+    }
 
     public Long getId() {
         return id;
@@ -27,6 +75,62 @@ public class Persona implements Serializable {
         this.id = id;
     }
 
+    public boolean isDiscapacitado() {
+        return discapacitado;
+    }
+
+    public void setDiscapacitado(boolean discapacitado) {
+        this.discapacitado = discapacitado;
+    }
+
+    public Calendar getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Calendar fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    public String getApellido_materno() {
+        return apellido_materno;
+    }
+
+    public void setApellido_materno(String apellido_materno) {
+        this.apellido_materno = apellido_materno;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 0;
@@ -49,7 +153,7 @@ public class Persona implements Serializable {
 
     @Override
     public String toString() {
-        return "org.itson.dominio.Persona[ id=" + id + " ]";
+        return "Persona{" + "rfc=" + rfc + ", nombres=" + nombres + ", apellido_paterno=" + apellido_paterno + ", apellido_materno=" + apellido_materno + '}';
     }
-    
+
 }
