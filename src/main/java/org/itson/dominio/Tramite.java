e9e1dd358edf05b051674827f5124fc9fb0eefae
Erumeru <elpichikoala@gmail.com>
2023-03-29 18:38:45 -0700

    Se creó el trámite padre del que hereda Trámite licencia y placas

---
+++ src/main/java/org/itson/dominio/Tramite.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.dominio;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author eruma
+ */
+@Entity
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+@Table(name="Tramite")
+public class Tramite implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable=false)
+    private Long id;
+
+    @Column(name="costo", nullable=false)
+    private int costo;
+    
+    @Column(name="fechaExpedicion", nullable=false)
+    private Calendar fechaExpedicion;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Tramite(Long id, int costo, Calendar fechaExpedicion) {
+        this.id = id;
+        this.costo = costo;
+        this.fechaExpedicion = fechaExpedicion;
+    }
+
+    public Tramite() {
+    }
+
+    public Tramite(int costo, Calendar fechaExpedicion) {
+        this.costo = costo;
+        this.fechaExpedicion = fechaExpedicion;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public Calendar getFechaExpedicion() {
+        return fechaExpedicion;
+    }
+
+    public void setFechaExpedicion(Calendar fechaExpedicion) {
+        this.fechaExpedicion = fechaExpedicion;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Tramite)) {
+            return false;
+        }
+        Tramite other = (Tramite) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.itson.dominio.TramitePlacas[ id=" + id + " ]";
+    }
+    
+}
